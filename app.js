14467cd728986f1d2726091214e1ee4b7d63a118
Noah <nmedoff123@gmail.com>
2016-02-17 11:50:15 -0800

    got functions and constructor to make the pizza shops

--- app.js
+++ app.js
@@ -1,39 +1,73 @@
 // JS file for pizza website
-// 'use strict';
-// var listFodder = [];
-// var placeForMyList = document.getElementbyId();
 
-// function listify(stuffToBeListed){
-// // the for loop for the array
-//   for(i = 0; i < listFodder.length; i++){
-//     var liE1 = document.createdElement('li');
-// // build an li element
-//     liE1.textContent = stuffToBeListed[i]
-// // put it in the dom
-//     placeForMyList.appendChild(liE1)
+// constructor function for pizza shops
+function PizzaShop(place, minPizza, maxPizza, minDelivery, maxDelivery){
+  this.place = place;
+  this.pizzaSold = 0;
+  this.deliveries = 0;
+  this.drivers = 0;
+  this.minPizza = minPizza;
+  this.maxPizza = maxPizza;
+  this.minDelivery = minDelivery;
+  this.maxDelivery = maxDelivery;
+}
+
+PizzaShop.prototype.setPizzaSold = function () {
+  var min = this.minPizza;
+  var max = this.maxPizza;
+  var pizzasSold = []
+  for(var i = 0; i < min.length; i++){
+    pizzasSold.push(Math.floor(Math.random() * (max[i] - min[i] + 1)) + min[i]);
+  }
+  this.pizzaSold = pizzasSold;
+};
+
+PizzaShop.prototype.setDeliveries = function () {
+  var min = this.minDelivery;
+  var max = this.maxDelivery;
+  var deliveriesMade = []
+  for(var i = 0; i < min.length; i++){
+    deliveriesMade.push(Math.floor(Math.random() * (max[i] - min[i] + 1)) + min[i]);
+  }
+  this.deliveries = deliveriesMade;
+};
+
+PizzaShop.prototype.setDrivers = function () {
+  var min = this.pizzaSold;
+  var max = this.deliveries;
+  var deliveryDrivers = [];
+  for(var i = 0; i < min.length; i++){
+    deliveryDrivers.push(Math.floor(Math.random() * (max[i] - min[i] + 1)) + min[i]);
+  }
+  this.drivers = deliveryDrivers;
+};
+
+// var tbEl = document.createElement('table');
+// PizzaShop.prototype.tableRowMaker = function(id, inputArray) {
+//   id = document.getElementById('');
+//   var trEl = document.createElement('tr');
+//   for(var i = 0; i < inputArray.length; i++) {
+//     var tdEl = document.createElement('td');
+//     tdEl.textContent = inputArray[i];
+//     trEl.appendChild(tdEl);
 //   }
-//
+//   tbEl.appendChild(trEl);
+//   id.appendChild(tbEl);
 // }
+// document.body.appendChild(tbEl)
 
-// Functions for the objects
-function howManyPizzas(min, max){
-  var amountOfPizzas = Math.floor(Math.random() * (max - min + 1)) + min;
-  this.pizza.push(amountOfPizzas);
-}
-function theDeliveries(){
-  var amountOfDeliveries =
-  this.deliveries.push(amountOfDeliveries)
-}
+var ballardMinPizza = [0, 0, 2, 15, 12, 5];
+var ballardMaxPizza = [4, 7, 15, 35, 31, 20];
+var ballardMinDelivery = [0, 0, 1, 3, 5, 6];
+var ballardMaxDelivery = [4, 4, 4, 8, 12, 11];
 
+var ballard = new PizzaShop('Ballard', ballardMinPizza, ballardMaxPizza, ballardMinDelivery, ballardMaxDelivery)
 
-// Array of times
-var timeArray = ['8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 pm', '8:00 pm', '9:00 pm', '10:00 pm', '11:00 pm', '12:00 am', '1:00 am'];
+var ballardRowOne = ['Time', 'Pizzas Sold', 'Pizzas Delivered', 'Drivers Recomennded']
+var ballardRowTwo = ['8am-11pm', ballard.pizzaSold, ballard.deliveries, ballard.drivers]
 
-// The object for the Ballard Pizza 3001 location
-var ballardShop = {
-  name:'Pizza 3001',
-  location: 'Ballard',
-  pizza: [0],
-  deliveries: [0],
-  drivers: [0]
-}
+ballard.setPizzaSold();
+ballard.setDeliveries();
+ballard.setDrivers();
+ballard.tableRowMaker('ballardtable', ballardRowOne)
+ballard.tableRowMaker('ballardtable', ballardRowTwo)
